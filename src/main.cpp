cedaae6d2931d98c4d910ce397a89893c4d0ec24
seung-gu <seunggu.kang.kr@gmail.com>
2024-10-21 22:19:41 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+
+int ledPin = 4;    // LED connected to digital pin 9
+int brightness = 0;   
+int fadeAmount = 5;   
+
+
+void setup() {
+  // nothing happens in setup
+}
+
+void loop() {
+  int delay_time = random(20, 100);
+  analogWrite(ledPin, constrain(brightness + random(-20, 20), 50, 175));   // LED 밝기 + 불규칙 요소
+  brightness = brightness + fadeAmount;
+  
+
+  if (delay_time%2 == 0) {
+    if (brightness <= 0){
+      fadeAmount = abs(fadeAmount);
+    }
+    else if(brightness >= 150){
+      fadeAmount = -abs(fadeAmount);
+    }
+    else{
+      fadeAmount = -fadeAmount;
+    }  
+  }
+
+  delay(delay_time);  // 랜덤한 딜레이
+}
